6221446936c7b71a55f717e0e4ca02df5edb49d6
Stepan Ananin <xrf848@gmail.com>
2025-11-17 21:05:18 +0300

    add docs in comments

--- packages/ascii/ascii.go
+++ packages/ascii/ascii.go
@@ -2,6 +2,8 @@ package ascii
 
 type Char = rune
 
+// Return true if given Char is one of ASCII control characters,
+// otherwise return false.
 func IsControlChar(c Char) bool {
 	return c <= 31
 }
--- packages/repl/cursor.go
+++ packages/repl/cursor.go
@@ -12,26 +12,32 @@ type cursor struct {
 
 }
 
+// Writes given char at current cursor position.
 func (c *cursor) WriteChar(char rune) *cursor {
 	fmt.Printf("%c", char)
 	return c
 }
 
+// Writes given string at current cursor position.
 func (c *cursor) Write(s string) *cursor {
 	fmt.Print(s)
 	return c
 }
 
+// Moves cursor to the beginning of line.
 func (c *cursor) Rewind() *cursor {
 	fmt.Print(ascii.CarriageReturn)
 	return c
 }
 
+// Moves cursor 1 character back.
 func (c *cursor) Back() *cursor {
 	fmt.Print(ansix364.Left)
 	return c
 }
 
+// Moves cursor N character(-s) back.
+// If N equals zero does nothing.
 func (c *cursor) BackN(n uint8) *cursor {
 	if n == 0 {
 		return c
@@ -40,11 +46,14 @@ func (c *cursor) BackN(n uint8) *cursor {
 	return c
 }
 
+// Moves cursor 1 character forward.
 func (c *cursor) Forward() *cursor {
 	fmt.Print(ansix364.Right)
 	return c
 }
 
+// Moves cursor N character(-s) forward.
+// If N equals zero does nothing.
 func (c *cursor) ForwardN(n uint8) *cursor {
 	if n == 0 {
 		return c
@@ -53,11 +62,14 @@ func (c *cursor) ForwardN(n uint8) *cursor {
 	return c
 }
 
+// Moves cursor 1 line up.
 func (c *cursor) Up() *cursor {
 	fmt.Print(ansix364.Up)
 	return c
 }
 
+// Moves cursor N line(-s) up.
+// If N equals zero does nothing.
 func (c *cursor) UpN(n uint8) *cursor {
 	if n == 0 {
 		return c
@@ -66,11 +78,14 @@ func (c *cursor) UpN(n uint8) *cursor {
 	return c
 }
 
+// Moves cursor 1 line down.
 func (c *cursor) Down() *cursor {
 	fmt.Print(ansix364.Down)
 	return c
 }
 
+// Moves cursor N line(-s) down.
+// If N equals zero does nothing.
 func (c *cursor) DownN(n uint8) *cursor {
 	if n == 0 {
 		return c
@@ -79,6 +94,7 @@ func (c *cursor) DownN(n uint8) *cursor {
 	return c
 }
 
+// Moves cursor to the beginning of new line.
 func (c *cursor) NewLine() *cursor {
 	fmt.Print(ascii.LineFeed)
 	return c
@@ -104,20 +120,25 @@ func (c *cursor) repeat(char rune, n uint8) *cursor {
 	return c
 }
 
+// Moves cursor to the beginning of new line, skipping N - 1 lines.
+// If N equals zero does nothing.
 func (c *cursor) NewLineN(n uint8) *cursor {
 	return c.repeat(ascii.LineFeed, n)
 }
 
+// Saves current cursor position.
 func (c *cursor) SavePosition() *cursor {
 	fmt.Print(ansix364.SavePosition)
 	return c
 }
 
+// Restores saved cursor position.
 func (c *cursor) RestorePosition() *cursor {
 	fmt.Print(ansix364.RestorePosition)
 	return c
 }
 
+// Removes all characters in inline and moves cursor to it's beginning.
 func (c *cursor) FlushLine() *cursor {
 	fmt.Print(ansix364.EraseAll+string(ascii.CarriageReturn))
 	return c
