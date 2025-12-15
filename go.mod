0dd0cf272529f2e856b47c42458f3e58c5fc100b
Stepan Ananin <xrf848@gmail.com>
2025-12-15 22:03:17 +0300

    fix user I/O (using github.com/chzyer/readline)
    Terminal programming from scratch is pure agony and doesn't worth time
    spending on it. Better to use some existing solution. All this stuff
    with handling escape sequences and state tracking is awful: edgecases
    everywhere and there are no way to normaly debug anything, printing
    stuff ruins app behaviour and in my case even debugger refused to work
    correctly, so fuck it.

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.24.0
 toolchain go1.24.3
 
 require (
+	github.com/chzyer/readline v1.5.1
 	github.com/traefik/yaegi v0.16.1
-	golang.org/x/term v0.37.0
 )
 
 require golang.org/x/sys v0.38.0 // indirect
--- packages/repl/repl.go
+++ packages/repl/repl.go
@@ -8,17 +8,14 @@ import (
 	"os"
 	"strings"
 
-	ansix364 "github.com/abaxoth0/Janus/packages/ansix3.64"
-	"github.com/abaxoth0/Janus/packages/ascii"
 	"github.com/abaxoth0/Janus/packages/interpreter"
-	"golang.org/x/term"
+	"github.com/chzyer/readline"
 )
 
-const InputPrefx string = "\r\n>>> "
+const InputPrefx string = ">>> "
 
 type REPL struct {
 	scanner     *bufio.Scanner
-	cursor      *cursor
 	inputReader *bufio.Reader
 	inputBuf    []rune
 }
@@ -26,106 +23,26 @@ type REPL struct {
 func New(r io.Reader) *REPL {
 	return &REPL{
 		scanner:     bufio.NewScanner(r),
-		cursor:      nil,
 		inputReader: bufio.NewReader(os.Stdin),
 	}
 }
 
-func (r *REPL) handleEscSeq(seq string) {
-	switch seq {
-	case ansix364.Left:
-		r.cursor.Back()
-	case ansix364.Right:
-		if r.cursor.GetX() < len(r.inputBuf)-1+len(InputPrefx) {
-			r.cursor.Forward()
-		}
-	default:
-		// panic(fmt.Sprintf("unsupported escape sequence: %q", seq))
-	}
-}
-
-func (r *REPL) readln() (string, error) {
-	r.inputBuf = r.inputBuf[:0]
-
-	const (
-		stateNormal = iota
-		stateEscape
-		stateCSI
-	)
-
-	state := stateNormal
-	csiBuffer := ""
-
-	for {
-		char, _, err := r.inputReader.ReadRune()
-		if err != nil {
-			return "", err
-		}
-
-		switch state {
-		case stateNormal:
-			switch {
-			case char == ascii.Escape:
-				state = stateEscape
-			case char == ascii.LineFeed || char == ascii.CarriageReturn:
-				return string(r.inputBuf), nil
-			case char == ascii.Backspace:
-				if len(r.inputBuf) > 0 {
-					r.inputBuf = r.inputBuf[:len(r.inputBuf)-1]
-					r.cursor.FlushChar()
-				}
-			case ascii.IsControlChar(char):
-				// ignore other (unhandled) control chars
-			default:
-				r.inputBuf = append(r.inputBuf, char)
-				r.cursor.WriteChar(char)
-			}
-		case stateEscape:
-			if char == ansix364.CSIPrefix {
-				state = stateCSI
-				csiBuffer = ""
-			} else {
-				// Reset if not a CSI sequence
-				// (currently there are no other supported escape sequences)
-				state = stateNormal
-			}
-		case stateCSI:
-			csiBuffer += string(char)
-
-			// CSI sequences end with a letter command
-			if ascii.IsAlpha(char) {
-				fullSeq := "\033[" + csiBuffer
-				r.handleEscSeq(fullSeq)
-				csiBuffer = ""
-				state = stateNormal
-			} else if len(csiBuffer) > 10 { // Safety limit
-				state = stateNormal
-				csiBuffer = ""
-			}
-		}
-	}
-}
-
 func (r *REPL) Run(interp interpreter.Interpreter) error {
-	stdin := int(os.Stdin.Fd())
-
-	oldState, err := term.MakeRaw(stdin)
+	rl, err := readline.New(InputPrefx)
 	if err != nil {
 		return err
 	}
-	defer term.Restore(stdin, oldState)
-
-	r.cursor = newCursor(len(InputPrefx) - 1).Rewind()
+	defer rl.Close()
 
 	for {
-		fmt.Print(InputPrefx)
-
-		line, err := r.readln()
+		line, err := rl.Readline()
 		if err != nil {
+			if err == io.EOF {
+				break
+			}
 			return err
 		}
 		if line == "" {
-			r.cursor.Rewind()
 			continue
 		}
 
@@ -133,7 +50,7 @@ func (r *REPL) Run(interp interpreter.Interpreter) error {
 			res := r.exec(line)
 			if res.Err != nil {
 				if res.Err == errInvalidCmd {
-					r.cursor.NewLine().Write("Invalid command: " + line)
+					fmt.Printf("Invalid command: \"%s\"\n", line)
 					continue
 				}
 				return res.Err
@@ -148,10 +65,10 @@ func (r *REPL) Run(interp interpreter.Interpreter) error {
 
 		val, err := interp.Eval(line)
 		if err != nil {
-			r.cursor.Writeln("Error:" + err.Error())
+			fmt.Println("Error:" + err.Error())
 		}
 		if val.IsValid() {
-			r.cursor.NewLine().RawWrite(val)
+			fmt.Println(val)
 		}
 	}
 
